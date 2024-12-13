e4ab6dbabd68a5e038a967978aa62c123eb2e781
Jung SeokYoung <thrdud0423@gmail.com>
2024-12-14 01:03:59 +0900

    [FEAT]: UIImage에 가로로 이미지 concat시키는 메서드 구현

--- Sources/VideoTrimmingSliderBar/Extensions/UIImage+Extension.swift
+++ Sources/VideoTrimmingSliderBar/Extensions/UIImage+Extension.swift
@@ -23,3 +23,61 @@ extension UIImage {
     return self.withTintColor(color, renderingMode: .alwaysOriginal)
   }
 }
+
+extension Array where Element: UIImage {
+  func concatImagesHorizontaly() -> UIImage {
+    let maxWidth = self.compactMap { $0.size.width }.max()
+    let maxHeight = self.compactMap { $0.size.height }.max()
+    
+    let maxSize = CGSize(width: maxWidth ?? 0, height: maxHeight ?? 0)
+    let totalSize = CGSize(width: maxSize.width * (CGFloat)(self.count), height: maxSize.height)
+    print(totalSize)
+    return UIGraphicsImageRenderer(size: totalSize).image { context in
+      for (index, image) in self.enumerated() {
+        
+        let rect = CGRect(
+          x: maxSize.width * CGFloat(index),
+          y: 0,
+          width: maxSize.width,
+          height: maxSize.height
+        )
+
+        image.draw(in: rect)
+      }
+    }
+  }
+  
+  func concatImagesHorizontallyGPU() -> UIImage {
+    let context = CIContext(options: [CIContextOption.useSoftwareRenderer: false])
+
+    let ciImages = self.compactMap { CIImage(image: $0) }
+
+    guard !ciImages.isEmpty else { return UIImage() }
+
+    let maxWidth = ciImages.compactMap { $0.extent.width }.max() ?? 0
+    let maxHeight = ciImages.compactMap { $0.extent.height }.max() ?? 0
+    
+    let maxSize = CGSize(width: maxWidth, height: maxHeight)
+    let totalSize = CGSize(
+      width: maxSize.width * (CGFloat)(self.count),
+      height: maxSize.height
+    )
+    let finalRect = CGRect(origin: .zero, size: totalSize)
+    
+    let outputImage = ciImages.enumerated().reduce(CIImage()) { result,  element in
+      let (index, image) = element
+      let xOffset = maxWidth * CGFloat(index)
+      
+      let translatedImage = image.transformed(by: CGAffineTransform(
+          translationX: xOffset,
+          y: (maxHeight - image.extent.height) / 2
+        )
+      )
+      return result.composited(over: translatedImage)
+    }
+    
+    // 결과를 UIImage로 변환
+    guard let cgImage = context.createCGImage(outputImage, from: finalRect) else { return UIImage() }
+    return UIImage(cgImage: cgImage)
+  }
+}
