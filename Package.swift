d8e3f44c5c6c34dc962d859987193fcb7e750fdf
Jung SeokYoung <thrdud0423@gmail.com>
2024-12-14 00:21:37 +0900

    [FEAT]: 프로젝트 기초 세팅

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "VideoTrimmingSliderBar",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "VideoTrimmingSliderBar",
+            targets: ["VideoTrimmingSliderBar"]
+        ),
+    ],
+    targets: [
+        .target(name: "VideoTrimmingSliderBar"),
+    ]
+)
