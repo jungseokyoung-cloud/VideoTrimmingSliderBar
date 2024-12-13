bf2213fa402a9390e2f61531a39f152eceace9d3
Jung SeokYoung <thrdud0423@gmail.com>
2024-12-14 00:33:15 +0900

    [CHORE]: SnapKit SPM 추가

--- Package.swift
+++ Package.swift
@@ -5,14 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "VideoTrimmingSliderBar",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "VideoTrimmingSliderBar",
             targets: ["VideoTrimmingSliderBar"]
-        ),
+        )
+    ],
+    dependencies: [
+      .package(name: "SnapKit", url: "https://github.com/SnapKit/SnapKit.git", .exactItem("5.0.1"))
     ],
     targets: [
-        .target(name: "VideoTrimmingSliderBar"),
+      .target(
+        name: "VideoTrimmingSliderBar",
+        dependencies: [
+          .byName(name: "SnapKit")
+        ]
+      ),
     ]
 )
